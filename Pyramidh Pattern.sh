83f2a2c7aaf866b6e61437dd2ab9a3fd436ce392
SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2024-10-07 02:04:20 +0600

    Create Pyramidh Pattern.sh

---
+++ Pyramidh Pattern.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Main script to print pyramid pattern
+read -p "Enter the number of rows N: " N
+
+for ((i = 1; i <= N; i++)); do
+    # Print spaces
+    for ((j = i; j < N; j++)); do
+        echo -n " "
+    done
+    # Print stars
+    for ((k = 1; k <= (2 * i - 1); k++)); do
+        echo -n "*"
+    done
+    echo ""
+done
